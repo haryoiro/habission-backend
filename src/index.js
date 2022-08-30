66f4d7b62ed059fd1add475511648eba17504df4
haryoiro <mizusecocolte@gmail.com>
2022-08-30 17:25:11 +0900

    add init server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  try {
+    res.send({ name: "hoge" });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.listen({ port: 3000 }, () => {
+  console.log(`Server ready at http://localhost:3000`);
+});
+console.log('starts');
+
+export default app;
